0e117b6512d7294291282aac0431096f2759195c
freeyy <shiyey@uci.edu>
2020-11-27 19:59:30 -0800

    project

---
+++ Assets/Scripts/ClickableTileScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Linked to the tile 3D objects in Unity
+ */
+public class ClickableTileScript : MonoBehaviour    
+{
+    //The x and y co-ordinate of the tile
+    public int tileX;
+    public int tileY;
+    
+    public GameObject unitOnTile;  // The unit on the tile
+    public tileMapScript map;
+}
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+/*
+ * Tile structure within the tileMapScript
+ */
+public class Tile
+{
+
+    public string name;
+    public GameObject tileVisualPrefab;
+    public GameObject unitOnTile;
+    public float movementCost = 1;
+    public bool isWalkable=true;
+    
+    /*
+    private int x;
+    private int y;
+
+    
+    public Tile( int xLocation, int yLocation)
+    {
+        x = xLocation;
+        y = yLocation;
+    }
+
+    public void setCoords(int xLocation, int yLocation)
+    {
+        x = xLocation;
+        y = yLocation;
+    }
+    */
+}
